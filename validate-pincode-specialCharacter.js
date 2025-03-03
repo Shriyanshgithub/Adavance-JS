70e3a396f59c526cce0586ab41d7714b5fe28739
Shriyansh Gupta <gshriyansh703@gmail.com>
2025-03-03 14:53:00 +0530

    [Shriyansh Gupta] Add. Validate pincode containing special character

---
+++ validate-pincode-specialCharacter.js
@@ -0,0 +1,16 @@
+const array = [400023 , 400088 , 253474573, 123456 , "A400088","4000@88B"]
+
+let pinRegex =  RegExp('^[0-9]{6}$');
+
+const validPinCode = (pincode) =>{
+    if(pinRegex.test(pincode)){
+        console.log("valid pincode : " + pincode);
+    }
+    else{
+        console.log("invalid pincode : " + pincode)
+    }
+}
+
+for(let i = 0 ; i< array.length ; i++){
+    validPinCode(array[i]);
+}
